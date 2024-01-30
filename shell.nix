fbfffbae87fa3c4181728157979fc325eb01837d
Adam Zhang <898544@lcps.org>
2024-01-30 14:06:29 -0500

    add nodejs

--- shell.nix
+++ shell.nix
@@ -1,5 +1,20 @@
 {pkgs ? import <nixpkgs> {}}:
 with pkgs;
   mkShell {
-    buildInputs = [(python3.withPackages (ps: with ps; [numpy matplotlib scikit-learn tensorflow keras pandas seaborn])) pyright black];
+    buildInputs = [
+      (python3.withPackages (ps:
+        with ps; [
+          numpy
+          matplotlib
+          scikit-learn
+          tensorflow
+          keras
+          pandas
+          seaborn
+        ]))
+      pyright
+      black
+
+      nodejs
+    ];
   }
